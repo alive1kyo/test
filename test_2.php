2b46ca28507e4a1ac9afa92d816393c0e6e3b2fd
alive1kyo <alive1kyo@gmail.com>
2019-02-21 13:47:40 +0900

    2019 copied to 41

---
+++ test_2.php
@@ -0,0 +1,16 @@
+<?php
+    $a = "def";
+    echo "<!DOCTYPE html>\n
+
+ ?>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>test_2</title>
+</head>
+<body>
+  <?php
+    printf("abc%sghi\n",$a);
+  ?>
+</body>
+</html>
