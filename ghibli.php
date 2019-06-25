c528eadae5d9b61899f47765ef71d270775609c5
alive1kyo <alive1kyo@gmail.com>
2019-06-25 15:50:30 +0900

    Create ghibli.php

---
+++ ghibli.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+</head>
+<body>
+<table width="100%" border="0" cellpadding="0" cellspacing="0" class="songTable mgt10 zeblaTable" summary="歴代ジブリ映画作品一覧">
+  <colgroup><col width="25%">
+  <col width="45%">
+  <col width="30%">
+  </colgroup>
+  <tbody><tr>
+    <th>公開日</th>
+    <th>タイトル</th>
+    <th>興行収入</th>
+  </tr>
+  <tr class="odd">
+    <td>1986年8月2日</td>
+    <td class="bold2"><a href="https://nendai-ryuukou.com/1980/movie1/103.html">天空の城ラピュタ</a></td>
+    <td>5.8億円</td>
+  </tr>
+  <tr>
+    <td>1988年4月16日</td>
+    <td class="bold2"><a href="https://nendai-ryuukou.com/1980/movie1/104.html">となりのトトロ</a></td>
+    <td>5.9億円</td>
+  </tr>
+  <tr class="odd">
+    <td>1988年4月16日</td>
+    <td class="bold2"><a href="https://nendai-ryuukou.com/1980/movie1/105.html">火垂るの墓</a></td>
+    <td>5.9億円</td>
+  </tr>
+  <tr>
+    <td>1989年7月29日</td>
+    <td class="bold2"><a href="https://nendai-ryuukou.com/1980/movie1/046.html">魔女の宅急便</a></td>
+    <td>21.5億円</td>
+  </tr>
+  <tr class="odd">
+    <td>1991年7月20日</td>
+    <td class="bold2"><a href="https://nendai-ryuukou.com/1990/movie1/006.html">おもひでぽろぽろ</a></td>
+    <td>18.7億円</td>
+  </tr>
+  <tr>
+    <td>1992年7月18日</td>
+    <td class="bold2"><a href="https://nendai-ryuukou.com/1990/movie1/011.html">紅の豚</a></td>
+    <td>28億円</td>
+  </tr>
+  <tr class="odd">
+    <td>1994年7月16日</td>
+    <td class="bold2"><a href="https://nendai-ryuukou.com/1990/movie1/021.html">平成狸合戦ぽんぽこ</a></td>
+    <td>26.5億円</td>
+  </tr>
+  <tr>
+    <td>1995年7月15日</td>
+    <td class="bold2"><a href="https://nendai-ryuukou.com/1990/movie1/026.html">耳をすませば</a></td>
+    <td>18.5億円</td>
+  </tr>
+  <tr class="odd">
+    <td>1997年7月12日</td>
+    <td class="bold2"><a href="https://nendai-ryuukou.com/1990/movie1/036.html">もののけ姫</a></td>
+    <td>193億円</td>
+  </tr>
+  <tr>
+    <td>1999年7月17日</td>
+    <td class="bold2"><a href="https://nendai-ryuukou.com/1990/movie1/099.html">ホーホケキョ となりの山田くん</a></td>
+    <td>7.9億円</td>
+  </tr>
+  <tr class="odd">
+    <td>2001年7月20日</td>
+    <td class="bold2"><a href="https://nendai-ryuukou.com/2000/movie1/006.html">千と千尋の神隠し</a></td>
+    <td>304億円</td>
+  </tr>
+  <tr>
+    <td>2002年7月20日</td>
+    <td class="bold2"><a href="https://nendai-ryuukou.com/2000/movie1/011.html">猫の恩返し</a></td>
+    <td>64.6億円</td>
+  </tr>
+  <tr class="odd">
+    <td>2004年11月20日</td>
+    <td class="bold2"><a href="https://nendai-ryuukou.com/2000/movie1/021.html">ハウルの動く城</a></td>
+    <td>196億円</td>
+  </tr>
+  <tr>
+    <td>2006年7月29日</td>
+    <td class="bold2"><a href="https://nendai-ryuukou.com/2000/movie1/031.html">ゲド戦記</a></td>
+    <td>76.5億円</td>
+  </tr>
+  <tr class="odd">
+    <td>2008年7月19日</td>
+    <td class="bold2"><a href="https://nendai-ryuukou.com/2000/movie1/041.html">崖の上のポニョ</a></td>
+    <td>155億円</td>
+  </tr>
+  <tr>
+    <td>2010年7月17日</td>
+    <td class="bold2"><a href="https://nendai-ryuukou.com/2010/movie1/001.html">借りぐらしのアリエッティ</a></td>
+    <td>92.5億円</td>
+  </tr>
+  <tr class="odd">
+    <td>2011年7月16日</td>
+    <td class="bold2"><a href="https://nendai-ryuukou.com/2011/movie1/001.html">コクリコ坂から</a></td>
+    <td>44.6億円</td>
+  </tr>
+  <tr>
+    <td>2013年7月20日</td>
+    <td class="bold2"><a href="https://nendai-ryuukou.com/2013/movie1/001.html">風立ちぬ</a></td>
+    <td>116.1億円</td>
+  </tr>
+  <tr class="odd">
+    <td>2013年11月23日</td>
+    <td class="bold2"><a href="https://nendai-ryuukou.com/2014/movie1/012.html">かぐや姫の物語</a></td>
+    <td>24.7億円</td>
+  </tr>
+  <tr>
+    <td>2014年7月19日</td>
+    <td class="bold2"><a href="https://nendai-ryuukou.com/2014/movie1/009.html">思い出のマーニー</a></td>
+    <td>35.3億円</td>
+  </tr>
+  <tr class="odd">
+    <td>2016年9月17日</td>
+    <td class="bold2"><a href="https://nendai-ryuukou.com/2016/movie1/032.html">レッドタートル ある島の物語</a></td>
+    <td>&nbsp;</td>
+  </tr>
+</tbody></table>
+
+</body>
+</html>
