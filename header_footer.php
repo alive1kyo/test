0a56a6617935fe98c9b497d063d7efb974bcd034
alive1kyo <alive1kyo@gmail.com>
2019-08-02 17:14:48 +0900

    ヘッダーとフッターのみ

---
+++ header_footer.php
@@ -0,0 +1,36 @@
+<?php
+// header and footer
+
+$HtmlHeader =<<< EOD
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+
+<title>title</title>
+
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+<link rel="stylesheet" type="text/css" href="css/table4.css" />
+  
+  	<style type="text/css">
+		a { text-decoration: none;
+			color: black;}
+	</style>
+  
+  </head>
+<body>
+<div id=''> <!-- wrapper -->
+EOD;
+// ---
+$HtmlFooter =<<< EOD
+  <div class='nopri'>
+  <hr />
+  <p class="footer">&copy;2019 kets Co.Ltd. & RyoTajiri (since 2014)</p>
+  </div>
+</div> <!-- end of wrapper -->
+
+</body>
+</html>
+EOD;
+//------------------------------------------------------------------
